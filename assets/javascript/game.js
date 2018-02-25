3c00a046791a80e9377d47c130ae3af24e069ffa
Elisa Urda <urda.elisa@gmail.com>
2018-02-25 14:19:14 -0500

    updated js to fully working with reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,13 @@ $(document).ready(function () {
     var startValue = 0;
     $("#userScore").html(startValue);
 
+    // display the total wins and losses as 0
+    var winTotal = 0;
+    $("#wins").html("Wins: " + winTotal);
+
+    var lossTotal = 0;
+    $("#losses").html("Losses: " + lossTotal);
+
     // function to choose a random number between the assigned numbers
     function randomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
@@ -30,31 +37,68 @@ $(document).ready(function () {
     // var blueNumber = $("#blue").attr("value", randomNumber(1, 12));
     // var redNumber = $("#red").attr("value", randomNumber(1, 12));
 
+    // reset function
+
+    function reset () {
+        startValue = 0
+        $("#userScore").html(startValue);
+        goalNumber = randomNumber(19, 120);
+        $("#assignedNumber").html(goalNumber);
+        console.log("New number " + goalNumber);
+        purpleNumber = randomNumber(1,12);
+        console.log("New purple " + purpleNumber);
+        greenNumber = randomNumber(1, 12);
+        console.log("New green " + greenNumber);
+        blueNumber = randomNumber(1, 12);
+        console.log("New blue " + blueNumber);
+        redNumber = randomNumber(1, 12);
+        console.log("New red " + redNumber);
+    }
+
+    // win and lose function
+
+    function winsLosses () {
+        if (startValue === goalNumber) {
+            $("#message").html("You won!");
+          winTotal++;
+          $("#wins").html("Wins: " + winTotal);
+            reset ();
+        }
+
+        else if (startValue > goalNumber) {
+            $("#message").html("You lose!");
+            lossTotal++;
+            $("#losses").html("Losses: " + lossTotal);
+            reset ();
+        };
+
+    }
+
 
     // on click take the value of the number assigned to the gem clicked and add it to the total of the user score
             $("#purple").on("click", function (){
                 startValue = startValue + purpleNumber;
                 $("#userScore").html(startValue);
+                winsLosses();
             });
 
             $("#green").on("click", function(){
                 startValue = startValue + greenNumber;
                 $("#userScore").html(startValue);
+                winsLosses();
             });
 
             $("#blue").on("click", function(){
                 startValue = startValue + blueNumber;
                 $("#userScore").html(startValue);
+                winsLosses();
             });
 
             $("#red").on("click", function(){
                 startValue = startValue + redNumber;
                 $("#userScore").html(startValue);
+                winsLosses();
             });
-
-            if (startValue === goalNumber) {
-                
-            }
 
     });
 
